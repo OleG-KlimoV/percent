a1081a6f45f93c7d5b2f6ef456d57eb8dd589a6f
OleG-KlimoV <122331966+OleG-KlimoV@users.noreply.github.com>
2023-01-27 15:21:09 +0800

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -17,3 +17,32 @@ if ticket > 3:
 else:
     discount = 1
     print('Сумма к оплате на', ticket, 'чел:', round(discount * price), 'рублёв')
+
+            
+### VAR2
+ticket = int(input('Укажите кол-во билетов : '))
+count, price = 0, 0
+for i in range(ticket):
+    while count <= i:
+            try:
+                count += 1
+                age = int(input(f'Возраст {count}-го посетителя: '))
+                # Если вводится не число, будет вызвано исключение:
+            except ValueError:
+                # Цикл будет повторяться до правильного ввода:
+                count -= 1
+                print("Нужно ввести число! Попробуйте снова...")
+                continue
+            else:
+                if age < 18:
+                    price += 0
+                elif 18 <= age < 25:
+                    price += 990
+                else:
+                    price += 1390
+                break
+    if ticket > 3:
+        discount = 0.9
+    else:
+        discount = 1
+print('Стоимость билетов на', ticket, 'чел:', round(discount * price), 'рублёв.')
